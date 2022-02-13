2c683e3bb7b2295f4f30fe27d11bbc10ee6187e6
DevGabrielPires <gabriel.r.pires@outlook.com>
2022-02-13 16:25:25 -0300

    :bug: Fix: Fixing some problems

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
-main() => runApp(PerguntaApp());
+main() => runApp(const PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
+  const PerguntaApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     final List<String> perguntas = [
@@ -13,20 +15,20 @@ class PerguntaApp extends StatelessWidget {
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
-        title: Text('Perguntas'),
+        title: const Text('Perguntas'),
       ),
       body: Column(
         children: [
           Text(perguntas.elementAt(0)),
-          ElevatedButton(
+          const ElevatedButton(
             child: Text('Resposta 1'),
             onPressed: null,
           ),
-          ElevatedButton(
+          const ElevatedButton(
             child: Text('Resposta 2'),
             onPressed: null,
           ),
-          ElevatedButton(
+          const ElevatedButton(
             child: Text('Resposta 3'),
             onPressed: null,
           ),
