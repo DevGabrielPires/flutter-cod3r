007f1dadb8462382712d582f6aa6e0d9fa4c67ec
devgabrielpires <gabriel.r.pires@outlook.com>
2022-03-21 16:50:57 -0300

    :sparkles: Feat: Finishing challenge  about conditional struture

---
+++ controle/desafio_if_else.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  var nota = 0.3;
+
+  if (nota >= 9) // não se usa ; em estrutura condicional *
+  {
+    print("Parabéns você foi aprovado");
+  }
+
+  // * EXCEÇÃO DO/WHILE!!!
+}
