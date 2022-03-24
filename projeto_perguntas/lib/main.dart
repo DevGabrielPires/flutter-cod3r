e80ac55bac4883cc5d8a6b39eb07af29919553a7
devgabrielpires <gabriel.r.pires@outlook.com>
2022-03-24 13:45:02 -0300

    :sparkles: Feat: Finishing class about anonimous function

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -7,36 +7,42 @@ void main(List<String> args) {
 class PerguntaApp extends StatelessWidget {
   const PerguntaApp({Key? key}) : super(key: key);
 
+  void responder() {
+    print('pergunta respondida');
+  }
+
   @override
   Widget build(BuildContext context) {
     final List<String> perguntas = [
+      'Qual é a sua cor favorita?',
       'Qual é o seu animal favorito?',
-      'Qual é a sua cor favorita?'
     ];
-
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Perguntas'),
-        ),
-        body: Column(
-          children: [
-            Center(child: Text(perguntas[0])),
-            const ElevatedButton(
-              child: Text('Resposta 1'),
-              onPressed: null,
-            ),
-            const ElevatedButton(
-              child: Text('Resposta 2'),
-              onPressed: null,
-            ),
-            const ElevatedButton(
-              child: Text('Resposta 3'),
-              onPressed: null,
-            )
-          ],
-        ),
-      ),
+          appBar: AppBar(
+            title: const Text('Perguntas'),
+          ),
+          body: Column(
+            children: <Widget>[
+              Text(
+                perguntas[0],
+              ),
+              ElevatedButton(
+                child: const Text('Resposta 1'),
+                onPressed: responder,
+              ),
+              ElevatedButton(
+                child: const Text('Resposta 2'),
+                onPressed: () {
+                  print('Resposta 2 escolhida!');
+                },
+              ),
+              ElevatedButton(
+                child: const Text('Resposta 3'),
+                onPressed: () => {print("Resposta 3!!!!!!")},
+              )
+            ],
+          )),
     );
   }
 }
