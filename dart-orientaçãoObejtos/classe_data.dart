ab2e3f3a8f4366f9236954c10693e88ec69df91a
devgabrielpires <gabriel.r.pires@outlook.com>
2022-07-29 11:05:04 -0300

    :sparkles: feat: finishing class about constructors

--- dart-orientaçãoObejtos/classe_data.dart
+++ dart-orientaçãoObejtos/classe_data.dart
@@ -3,19 +3,28 @@ class Data {
   late int mes;
   late int ano;
 
+  // Data(int dia, int mes, int ano) {
+  //   this.dia = dia;
+  //   this.mes = mes;
+  //   this.ano = ano;
+  // }
+
+  //Faz a mesma coisa que o código comentado acima. porem de uma forma mais simples
+  Data([this.dia = 1, this.mes = 1, this.ano = 1970]);
+
   String obterFormatada() {
     return "$dia/$mes/$ano";
   }
 }
 
 void main() {
-  var dataAniversario = new Data();
+  var dataAniversario = new Data(3, 10, 2020);
 
-  dataAniversario.dia = 3;
-  dataAniversario.mes = 10;
-  dataAniversario.ano = 2020;
+  // dataAniversario.dia = 3;
+  // dataAniversario.mes = 10;
+  // dataAniversario.ano = 2020;
 
-  Data dataCompra = Data();
+  Data dataCompra = Data(1, 1, 1970);
 
   dataCompra.dia = 23;
   dataCompra.mes = 12;
@@ -24,4 +33,6 @@ void main() {
   print(dataAniversario.obterFormatada());
   String dataCompraFormatada = dataCompra.obterFormatada();
   print(dataCompraFormatada);
+
+  print(Data());
 }
